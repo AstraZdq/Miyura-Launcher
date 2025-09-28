0b6332748f738a09182f3152f0fe54c9ec8f0de3
AstraZdq <113625326+AstraZdq@users.noreply.github.com>
2025-09-28 06:46:18 +0200

    Update database.js

--- src/assets/js/utils/database.js
+++ src/assets/js/utils/database.js
@@ -4,18 +4,20 @@
  */
 
 const { NodeBDD, DataType } = require('node-bdd');
-const nodedatabase = new NodeBDD()
-const { ipcRenderer } = require('electron')
-
-let dev = process.env.NODE_ENV === 'dev';
+const nodedatabase = new NodeBDD();
+const { ipcRenderer } = require('electron');
 
 class database {
     async creatDatabase(tableName, tableConfig) {
+        // Récupère automatiquement le chemin %AppData%\Miyura-Launcher
+        let userDataPath = await ipcRenderer.invoke('path-user-data');
+        let pathDb = `${userDataPath}/databases`;
+
         return await nodedatabase.intilize({
             databaseName: 'Databases',
-            fileType: dev ? 'sqlite' : 'db',
+            fileType: 'db',
             tableName: tableName,
-            path: `${await ipcRenderer.invoke('path-user-data')}${dev ? '../..' : '/databases'}`,
+            path: pathDb,
             tableColumns: tableConfig,
         });
     }
@@ -28,43 +30,43 @@ class database {
 
     async createData(tableName, data) {
         let table = await this.getDatabase(tableName);
-        data = await nodedatabase.createData(table, { json_data: JSON.stringify(data) })
-        let id = data.id
-        data = JSON.parse(data.json_data)
-        data.ID = id
-        return data
+        data = await nodedatabase.createData(table, { json_data: JSON.stringify(data) });
+        let id = data.id;
+        data = JSON.parse(data.json_data);
+        data.ID = id;
+        return data;
     }
 
     async readData(tableName, key = 1) {
         let table = await this.getDatabase(tableName);
-        let data = await nodedatabase.getDataById(table, key)
+        let data = await nodedatabase.getDataById(table, key);
         if (data) {
-            let id = data.id
-            data = JSON.parse(data.json_data)
-            data.ID = id
+            let id = data.id;
+            data = JSON.parse(data.json_data);
+            data.ID = id;
         }
-        return data ? data : undefined
+        return data ? data : undefined;
     }
 
     async readAllData(tableName) {
         let table = await this.getDatabase(tableName);
-        let data = await nodedatabase.getAllData(table)
+        let data = await nodedatabase.getAllData(table);
         return data.map(info => {
-            let id = info.id
-            info = JSON.parse(info.json_data)
-            info.ID = id
-            return info
-        })
+            let id = info.id;
+            info = JSON.parse(info.json_data);
+            info.ID = id;
+            return info;
+        });
     }
 
     async updateData(tableName, data, key = 1) {
         let table = await this.getDatabase(tableName);
-        await nodedatabase.updateData(table, { json_data: JSON.stringify(data) }, key)
+        await nodedatabase.updateData(table, { json_data: JSON.stringify(data) }, key);
     }
 
     async deleteData(tableName, key = 1) {
         let table = await this.getDatabase(tableName);
-        await nodedatabase.deleteData(table, key)
+        await nodedatabase.deleteData(table, key);
     }
 }
 
